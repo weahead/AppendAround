0395c6069ab616f8cf8f1326455743afbc8aed5b
Michael Lopez <michael@weahead.se>
2015-09-24 09:54:29 +0200

    Single listener, one call to $.appendAround().

--- README.md
+++ README.md
@@ -2,42 +2,44 @@
 
 ## A pattern for responsive markup
 
-- [c]2012, @scottjehl, Filament Group, Inc. MIT/GPL 
+- [c]2012, @scottjehl, Filament Group, Inc. MIT/GPL
+
+- [c]2015, We ahead AB. MIT/GPL
 
 ## how-to
 	1. Insert potential element containers throughout the DOM
 	2. give each container a data-set attribute with a value that matches all other containers' values
 	3. Place your appendAround content in one of the potential containers
 	4. Configure your CSS to only display one potential container at a time (and display others depending on @media conditions in your CSS)
-	4. Call appendAround() on that element when the DOM is ready, and it'll keep itself in a visibile container at all times
+	4. Call $.appendAround() when the DOM is ready, and it'll keep all appendAround content in a visibile container at all times
 
 
 ## Sample markup
 
 	  <!-- potential container for appendAround -->
 	  <div class="foo" data-set="foobarbaz"></div>
-  
+
 	  <ul>
 	   <li>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</li>
 	   <li>Aliquam tincidunt mauris eu risus.</li>
 	   <li>Vestibulum auctor dapibus neque.</li>
     </ul>
-  
+
 	  <!-- potential container for appendAround -->
 	  <div class="bar" data-set="foobarbaz"></div>
-  
+
 	  <ul>
 	   <li>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</li>
 	   <li>Aliquam tincidunt mauris eu risus.</li>
 	   <li>Vestibulum auctor dapibus neque.</li>
 	</ul>
-  
-    
+
+
 	  <!-- initial container for appendAround -->
 	  <div class="baz" data-set="foobarbaz">
-  
+
 	    <p class="sample">Sample appendAround Element</p>
-  
+
 	  </div>
 
 ## Sample CSS
@@ -47,34 +49,34 @@
 		padding: 1em;
 		background: tan;
 	}
-  
+
 	.baz {
 		display: block;
 	}
 	.foo,
 	.bar {
-		display: none; 
+		display: none;
 	}
-  
+
 	@media (min-width: 30em){
 		.bar {
 		  display: block;
 		}
 		.foo, .baz {
-		  display: none; 
+		  display: none;
 		}
 	}
-  
+
 	@media (min-width: 50em){
 		div.foo {
 		  display: block;
 		}
 		div.bar, div.baz {
-		  display: none; 
+		  display: none;
 		}
 	}
 
 
 ## Sample JavaScript call
 
-    $( ".sample" ).appendAround();
+    $.appendAround();
--- appendAround.js
+++ appendAround.js
@@ -1,42 +1,81 @@
-/*! appendAround markup pattern. [c]2012, @scottjehl, Filament Group, Inc. MIT/GPL 
+/*! appendAround markup pattern. [c]2012, @scottjehl, Filament Group, Inc. MIT/GPL
+/*! appendAround markup pattern. [c]2015, We ahead AB. MIT/GPL
 how-to:
 	1. Insert potential element containers throughout the DOM
 	2. give each container a data-set attribute with a value that matches all other containers' values
 	3. Place your appendAround content in one of the potential containers
-	4. Call appendAround() on that element when the DOM is ready
+	4. Call $.appendAround() when the DOM is ready
 */
 (function( $ ){
-	$.fn.appendAround = function(){
-	  return this.each(function(){
-      
-	    var $self = $( this ),
-	        att = "data-set",
-	        $parent = $self.parent(), 
-	        parent = $parent[ 0 ],
-	        attval = $parent.attr( att ),
-	        $set = $( "["+ att +"='" + attval + "']" );
 
-		function isHidden( elem ){
-			return $(elem).css( "display" ) === "none";
+	var $window = $(window);
+	var listening = false;
+	var containers = {};
+	var children = {};
+	var timer;
+
+	function isHidden( elem ){
+		return $(elem).css( "display" ) === "none";
+	}
+
+	function getVisibleContainer(set) {
+		var i, n;
+		for (i = 0, n = containers[set].length; i < n; ++i) {
+			var container = containers[set][i];
+			if (!isHidden(container)) {
+				return container;
+			}
+		}
+		return null;
+	}
+
+	function appendToVisibleContainer() {
+		var set;
+		for (set in children) {
+			if (children.hasOwnProperty(set)) {
+				var content = children[set];
+				var container = getVisibleContainer(set);
+				if (container) {
+					container.append(content);
+					return true;
+				}
+			}
+		}
+	}
+
+	$.appendAround = function(){
+
+		if (!listening) {
+			$window.on('resize', function () {
+				appendToVisibleContainer();
+				// window.clearTimeout(timer);
+				// timer = window.setTimeout(function() {
+				// 	appendToVisibleContainer();
+				// }, 150);
+			});
+			listening = true;
 		}
 
-		function appendToVisibleContainer(){
-			if( isHidden( parent ) ){
-				var found = 0;
-				$set.each(function(){
-					if( !isHidden( this ) && !found ){
-						$self.appendTo( this );
-						found++;
-						parent = this;
-					}
-				});
-	      	}
-	    }
-      
-	    appendToVisibleContainer();
-      
-	    $(window).bind( "resize", appendToVisibleContainer );
-      
-	  });
+	  var att = "data-set";
+
+		$('[' + att + ']').each(function (i, e) {
+			var $el = $(e);
+			var set = $el.attr(att);
+
+			if (!containers[set]) {
+				containers[set] = [];
+			}
+			containers[set].push($el);
+
+			var $children = $el.children();
+			if ($children.length > 0) {
+				if (children[set]) {
+					throw 'Set "' + set + '" has children in multiple containers.';
+				}
+				children[set] = $children;
+			}
+		});
+
+    appendToVisibleContainer();
 	};
 }( jQuery ));
